7e7404e45da9afd546fc1e85176ebfac79bdc3cc
elmarariabderrahim <32686758+elmarariabderrahim@users.noreply.github.com>
2020-03-06 16:31:55 +0100

    Update add_to_container.sh

--- add_to_container.sh
+++ add_to_container.sh
@@ -4,7 +4,7 @@ IFS=':'
 read -ra ADDR <<< "$str"
 docker_mysql_port=${ADDR[1]}
 mysql -P $docker_mysql_port --protocol=tcp -u root -pHGKO$.xu1234 -Bse "drop database if exists db5;create database db5;"
-docker exec -i 42178149b9d4 mysql -uroot -pHGKO$.xu1234 db5 < ./db1clone.sql
+docker exec -i 42178149b9d4 mysql -uroot -pHGKO$.xu1234 db5 < ./propperdb.sql
 mysql -P $docker_mysql_port --protocol=tcp -uroot -pHGKO$.xu1234 -Bse "use db5; select * from Persons;"
 for f in sql_scripts/*; do
 input="./$f"
