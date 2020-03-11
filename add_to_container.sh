45e17d0125b510000f93e14c4682fd730c82de46
elmarariabderrahim <32686758+elmarariabderrahim@users.noreply.github.com>
2020-03-11 16:40:55 +0100

    Update add_to_container.sh

--- add_to_container.sh
+++ add_to_container.sh
@@ -6,13 +6,4 @@ docker_mysql_port=${ADDR[1]}
 mysql -P $docker_mysql_port --protocol=tcp -u root -pHGKO$.xu1234 -Bse "drop database if exists db5;create database db5;"
 docker exec -i 42178149b9d4 mysql -uroot -pHGKO$.xu1234 db5 < ./dbpro.sql
 mysql -P $docker_mysql_port --protocol=tcp -uroot -pHGKO$.xu1234 -Bse "use db5;"
-for f in sql_scripts/*; do
-input="./$f"
-varrr=""
-while IFS= read -r line
-do
-varrr="${varrr}$line"
-done < "$input"
-echo "$varrr"
-mysql -P $docker_mysql_port --protocol=tcp -uroot -pHGKO$.xu1234 -Bse "$varrr"
-done
+
